6560d1547274b40b5010a44b39d982bc2c0042ca
scfulga <146138900+scfulga@users.noreply.github.com>
2024-03-10 22:53:25 +0200

    Added dwm vacanttags patch

--- dwm/config.h
+++ dwm/config.h
@@ -33,7 +33,7 @@ static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 0;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = {"JetBrains Mono:Regular:size=8", "monospace:size=8", "fontawesome:size=8"};
+static const char *fonts[]          = {"JetBrains Mono:Regular:size=8", "monospace:size=11", "fontawesome:size=11"};
 static const char dmenufont[]       = "JetBrains Mono:Regular:size=8";
 
 //background color
@@ -66,7 +66,6 @@ static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "firefox",  NULL,       NULL,       1 << 0,       0,           -1 },
 	{ "discord",  NULL, 	  NULL,       1 << 0,       0,            1 },
 	{ "Signal",  "signal", 	  NULL,       1 << 2,       0,           -1 },
@@ -76,9 +75,9 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact     = 0.5; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;/* 1 means respect size hints in tiled resizals */
-static const int attachbelow = 1;    /* 1 means attach after the currently active window */
+static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
+static const int attachbelow = 1;    /* 1 means attach after the currently active window */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
@@ -152,10 +151,10 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
-	 TAGKEYS(                        XK_4,                      3) 
-	 TAGKEYS(                        XK_5,                      4) 
-	 TAGKEYS(                        XK_6,                      5) 
-	 TAGKEYS(                        XK_7,                      6) 
+	TAGKEYS(                        XK_4,                      3)
+	TAGKEYS(                        XK_5,                      4)
+	TAGKEYS(                        XK_6,                      5)
+	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|Mod1Mask,              XK_q,      quit,           {0} },
@@ -167,7 +166,6 @@ static const Button buttons[] = {
 	/* click                event mask      button          function        argument */
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
-//	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
