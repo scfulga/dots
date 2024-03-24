583fccd909c4c3c0c8e08b47e8acf3551fdc0d94
scfulga <146138900+scfulga@users.noreply.github.com>
2024-03-24 22:26:00 +0200

    Changed font in st

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrains Mono:Regular:pixelsize=16:antialias=true:autohint=true";
+static char *font = "Fantasque Sans Mono:pixelsize=20:antialias=true:autohint=true";
 static int borderpx = 2;
 
 float alpha = 0.95;
